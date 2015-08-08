a4073244f33df0b58dfe07f3b728c8653ec301e8
Oleh <olehchem@ukr.net>
2015-08-08 14:54:33 +0300

    Init

---
+++ public/javascripts/script.js
@@ -0,0 +1,249 @@
+var appearanceSettings = [{
+	mainItem: "Appearance",
+	subItems: null
+}, {
+	mainItem: "Background and plot area",
+	subItems: ["Plot Area", "Margins", "Background and Border"]
+}, {
+	mainItem: "General Settings",
+	subItems: null
+}, {
+	mainItem: "Miscellaneous",
+	subItems: ["Other", "Columns", "Zooming", "Animation"]
+}, {
+	mainItem: "Number formatting",
+	subItems: null
+}, {
+	mainItem: "Axis",
+	subItems: null
+}];
+var dataAppearanceMatching = {
+	"axis": {
+		header: "y",
+		inputs: [{
+			"label": "Show",
+			"type": "checkbox",
+			"id": "show",
+			"checked": true
+		}, {
+			"label": "Max",
+			"type": "text",
+			"id": "max",
+			"value": undefined
+		}, {
+			"mult": true,
+			"subname": "label",
+			"elements": [{
+				"label": "Content",
+				"type": "text",
+				"id": "text",
+				"value": "Y Label"
+			}, {
+				"label": "Position",
+				"type": "text",
+				"id": "position",
+				"value": "outer-middle"
+			}]
+		}]
+	},
+	"appearance": {
+
+	}
+};
+var chartData = {
+	bindto: '#chart',
+	axis: {
+		y: {
+			label: {
+				text: 'Y Label',
+				position: 'outer-middle'
+			},
+			show: true,
+			max: undefined
+		}
+	},
+	data: {
+		json: [{
+			name: 'www.site1.com',
+			upload: 200,
+			download: 200,
+			total: 400
+		}, {
+			name: 'www.site2.com',
+			upload: 100,
+			download: 300,
+			total: 400
+		}, {
+			name: 'www.site3.com',
+			upload: 300,
+			download: 200,
+			total: 500
+		}, {
+			name: 'www.site4.com',
+			upload: 400,
+			download: 100,
+			total: 500
+		}, ],
+		keys: {
+			y: 'name',
+			value: ['upload', 'download'],
+		}
+	}
+};
+
+function appearToJSON(root, appear, targetValues) {
+	var json = {};
+	json[root] = {};
+	json[root][appear.header] = {};
+	for (var i = 0; i < appear.inputs.length; ++i) {
+		if (appear.inputs[i].mult) {
+			json[root][appear.header][appear.inputs[i].subname] = {};
+			for (var j = 0; j < appear.inputs[i].elements.length; ++j) {
+				var id = '#' + appear.inputs[i].elements[j].id;
+				console.log(id);
+				json[root][appear.header][appear.inputs[i].subname][appear.inputs[i].elements[j].id] = targetValues.find(id).val();
+			}
+		} else {
+
+			var id = '#' + appear.inputs[i].id;
+			console.log(id);
+			if (appear.inputs[i].type === "checkbox") {
+				json[root][appear.header][appear.inputs[i].id] = targetValues.find(id).is(':checked');
+			} else {
+				json[root][appear.header][appear.inputs[i].id] = targetValues.find(id).val();
+			}
+		}
+	}
+	return json;
+}
+var ExtendedMenuModel = Backbone.Model.extend({});
+var extendedMenuModel = new ExtendedMenuModel(chartData);
+
+var SettingsView = Backbone.View.extend({
+	appearance: {},
+	currentItem: {},
+	initialize: function(options) {
+		this.appearance = options.appearance;
+		this.render();
+	},
+	render: function() {
+		var template = _.template($("#settingsTemplate").html());
+		var resultHTML = template({
+			appear: this.appearance
+		});
+		this.$el.html(resultHTML);
+	},
+	events: {
+		"click .subitem": "changeExtendedMenu"
+	},
+	changeExtendedMenu: function(e) {
+		var currentElement = $(e.target);
+
+		var subMenu = currentElement.children("ul");
+		if (subMenu) {
+			subMenu.addClass("show");
+			subMenu.removeClass("hidden");
+		}
+		var idText = currentElement.text().trim().toLowerCase();
+
+		var extendMenu = new ExtendedMenuView({
+			el: $("#extended"),
+			appearance: dataAppearanceMatching[idText],
+			model: extendedMenuModel,
+			idText: idText
+		});
+	}
+});
+
+var ExtendedMenuView = Backbone.View.extend({
+	appearance: {},
+	idText: "",
+	initialize: function(options) {
+		this.appearance = options.appearance;
+		this.render();
+		this.idText = options.idText;
+	},
+	render: function() {
+		var template = _.template($("#extendedMenuTemplate").html());
+		var resultHTML = template({
+			data: this.appearance
+		});
+		this.$el.html(resultHTML);
+	},
+	events: {
+		"submit": "submit"
+	},
+	submit: function(e) {
+		e.preventDefault();
+		var currentData = _.clone(this.model.get(this.idText));
+		var currentElement = $(e.currentTarget);
+		//console.log($(currentElement).find("#position"));
+		var futureData = appearToJSON(this.idText, dataAppearanceMatching[this.idText], currentElement).axis;
+		console.log(currentData);
+		console.log(futureData);
+		this.model.set(this.idText, futureData);
+		this.model.trigger("change");
+	}
+
+});
+
+var ChartView = Backbone.View.extend({
+	initialize: function() {
+		this.model.bind('change', this.render, this);
+		this.render();
+	},
+	render: function() {
+		var chart = c3.generate(this.model.toJSON());
+	}
+});
+
+
+var DataView = Backbone.View.extend({
+	initialize: function() {
+		this.model.bind('change', this.render, this);
+		this.render();
+	},
+	render: function() {
+		var template = _.template($("#dataTemplate").html());
+		var resultHTML = template({
+			data: this.model.get("data")
+		});
+		this.$el.html(resultHTML);
+	}
+});
+
+function initApp() {
+	var chart = new ChartView({
+		el: $("#chart"),
+		model: extendedMenuModel
+	});
+	var settings = new SettingsView({
+		el: $("#settings"),
+		appearance: appearanceSettings
+	});
+	var data = new DataView({
+		el: $("#data"),
+		model: extendedMenuModel
+
+	});
+};
+
+initApp();
+
+function save() {
+	var request = $.ajax({
+		url: "/save",
+		async: true,
+		type: "POST",
+		data: chartData,
+		contentType: "application/x-www-form-urlencoded", //This is what made the difference.
+		dataType: "json",
+
+	});
+	request.success(function(result) {
+		console.log(result);
+	});
+	request.fail(function(jqXHR, textStatus) {
+		alert("Request failed: " + textStatus);
+	});
+}
---
+++ routes/index.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var passport = require("passport");
+var passportLocal = require("passport-local");
+var router = express.Router();
+var encrypt = require('../secure/encrypt');
+var db = require('../database/db');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', {
+		isAuthenticated: req.isAuthenticated(),
+		user: req.user
+	});
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+	/* Entered login or password is incorrect */
+	var message = req.query.valid ? "Incorrect password or email" : null;
+	res.render('login', {
+		errorInfo: message
+	});
+});
+/* POST user data. */
+router.post('/login', function(req, res, next) {
+	/* Passport function */
+	passport.authenticate('local', function(err, user, info) {
+		/* Error */
+		if (err) {
+			return next(err);
+		}
+		/* Incorrect login or password */
+		if (!user) {
+			var string = encodeURIComponent('something that would break');
+			return res.redirect('/login?valid=' + string);
+		}
+		/* Correct login and password */
+		req.logIn(user, function(err) {
+			if (err) {
+				return next(err);
+			}
+			return res.redirect('/');
+		});
+	})(req, res, next);
+});
+/* Logout and redirect to home page */
+router.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+/* Registration */
+router.get('/registration', function(req, res) {
+	res.render('registration');
+});
+/* Registrate new user */
+router.post('/registration', function(req, res) {
+	var user = {
+			name: req.body.name,
+			email: req.body.email,
+			passport: encrypt.encrypt(req.body.password),
+			charts: []
+		};
+	db.insertUser(user);
+	res.redirect('/');
+});
+router.post('/save', function(req, res) {
+	
+	console.log(req.isAuthenticated());
+
+	if (req.isAuthenticated()) {
+		console.log(req.user.id);
+		var userID = req.user.id;
+		var chart = req.body;
+		db.saveChart(userID, chart);
+	} else {
+		console.log("Please login");
+	}
+	res.send(req.body);
+});
+router.get('/editor', function(req, res) {
+	res.render('editor');
+});
+router.get('/facebook',
+  passport.authenticate('facebook'),
+  function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+  });
+router.get('/facebook/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+module.exports = router;
