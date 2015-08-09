87300c065269a84c966bf699ec942713b48229e5
Oleh <olehchem@ukr.net>
2015-08-10 00:12:31 +0300

    user page added

---
+++ public/javascripts/chartsList.js
@@ -0,0 +1,60 @@
+var url = "/data";
+console.log("here");
+
+function success(data) {
+	var charts = [];
+	for (var i = 0; i < data.charts.length; ++i) {
+
+		charts.push(new ChartModel({
+			id: data.charts[i].id,
+			name: data.charts[i].name,
+			description: data.charts[i].description,
+			chart: data.charts[i].chartData
+
+		}));
+		console.log(charts[i]);
+		$('#container').append((new ChartView({
+			model: charts[i]
+		})).render().el);
+	}
+}
+var ChartModel = Backbone.Model.extend({
+	defaults: {
+		id: "",
+		name: "",
+		description: "",
+		chart: {}
+	},
+	urlRoot: "/charts"
+});
+var ChartView = Backbone.View.extend({
+	render: function() {
+		this.$el.html("" + this.model.get('name') + this.model.get('description') + "<input type='button' value='delete' id='delete'/>");
+		return this;
+	},
+	events: {
+		"click #delete": "deleteChart"
+	},
+	deleteChart: function() {
+		this.remove();
+        this.model.destroy();
+		// this.model.destroy({
+		// 	success: function(model, respose, options) {
+		// 		console.log("The model has deleted the server");
+		// 	},
+		// 	error: function(model, xhr, options) {
+		// 		console.log(xhr);
+		// 	}
+		// })
+	}
+});
+
+$.ajax({
+	dataType: "json",
+	url: url,
+	success: success,
+	error: function(request, error) {
+		console.log(arguments);
+		alert(" Can't do because: " + error);
+	}
+});
