71b24267b2ca8923df3db0ccc3872eff117344ca
Oleh <olehchem@ukr.net>
2015-08-15 14:15:52 +0300

    da

--- routes/index.js
+++ routes/index.js
@@ -27,21 +27,21 @@ router.get('/fac', function(req, res) {
 });
 // var accessToken = ''; 
 // // function postToFacebook(str, cb) {
-// //   var req = https.request({
-// //     host: 'graph.facebook.com',
-// //     path: '/me/feed',
-// //     method: 'POST'
-// //   }, function(res) {
-// //   	console.log('message='+encodeURIComponent(str)
-// //     +'&access_token='+encodeURIComponent('CAACESdBRI10BAN8h9ep0GoQP3hwgBdCmycXZBWXiUMikEZBeMnQ5BCtXNgg8pW4Mts1zwzEJVFF7vjO6KMr9Oh04rH7FhLfKSanRPTB6Enus3Jo0eiN8sk0ZCxwTPl4iR6oQ465JVERz4RMZAq03tLlX1S9cSeLHVaHzhVWBbbIKZBgZBlzHkWFeDqKSiFw6h6HcHZBDof4WQZDZD'));
-// //     res.setEncoding('utf8');
-// //     res.on('data', function(chunk) {
-// //       console.log('got chunk '+chunk);
-// //     });
-// //     res.on('end', function() {
-// //       console.log('response end with status '+ res.status);
-// //     });
-// //   });
+  // var req = https.request({
+  //   host: 'graph.facebook.com',
+  //   path: '/me/feed',
+  //   method: 'POST'
+  // }, function(res) {
+  // 	console.log('message='+encodeURIComponent(str)
+  //   +'&access_token='+encodeURIComponent('CAACESdBRI10BAN8h9ep0GoQP3hwgBdCmycXZBWXiUMikEZBeMnQ5BCtXNgg8pW4Mts1zwzEJVFF7vjO6KMr9Oh04rH7FhLfKSanRPTB6Enus3Jo0eiN8sk0ZCxwTPl4iR6oQ465JVERz4RMZAq03tLlX1S9cSeLHVaHzhVWBbbIKZBgZBlzHkWFeDqKSiFw6h6HcHZBDof4WQZDZD'));
+  //   res.setEncoding('utf8');
+  //   res.on('data', function(chunk) {
+  //     console.log('got chunk '+chunk);
+  //   });
+  //   res.on('end', function() {
+  //     console.log('response end with status '+ res.status);
+  //   });
+  // });
 // //   console.log("                                            Acess Token                      :" + accessToken);
 // //   req.end('message='+encodeURIComponent(str)
 // //     +'&access_token='+encodeURIComponent('CAACESdBRI10BAJp8PC4QPYPvTL87c3Mm3UUKGixj2H04MUFuG8JkNw2mVGNPsTnxAcIZADUc6cnFMxxWwVsSWIz4ZBtYp9dPN6MiQYuVTZC7dquNes7kuUrjEwDzBPAYCgnEVSi4ZC96usfshrvv5pZAtAcFEPRBANhyZAzNOPMRGZALWEQ0Cv64Cnx4YhLUb4jez6xZBhqu3QZDZD'));
@@ -52,44 +52,68 @@ router.get('/fac', function(req, res) {
 
 // var FACEBOOK_APP_ID = "145452562457437";
 // var FACEBOOK_APP_SECRET = "5e46e22927a26aa5d529975afed0be43";
-
+https://twitter.com/intent/tweet?url=http%3A%2F%2Flive.amcharts.com%2FmM222OD%2F&text=Build+your+own+free+chart+like+this+and+share+on+Twitter+or+add+as+interactive+widget+to+your+website.
 router.get('/login', function(req, res, next) {
 	/* Entered login or password is incorrect */
 	var message = req.query.valid ? "Incorrect password or email" : null;
 	res.render('login', {
 		errorInfo: message
 	});
-	// console.log("ACS" + req.user.accessTokens);
-	var FB = require('fb');
-
-	FB.api('oauth/access_token', {
-		client_id: '145452562457437',
-		client_secret: '5e46e22927a26aa5d529975afed0be43',
-		grant_type: 'client_credentials'
-	}, function(res) {
-		if (!res || res.error) {
-			console.log(!res ? 'error occurred' : res.error);
-			return;
-		}
-
-		var accessToken = res.access_token;
-
-		console.log("TESTT:"+accessToken);
-		FB.setAccessToken(accessToken);
-
-		var body = 'My first post using facebook-node-sdk';
-		FB.api('me/feed', 'post', {
-			message: body
-		}, function(res) {
-			console.log("TESTT:"+res.error);
-			if (!res || res.error) {
-				console.log(!res ? 'error occurred' : res.error);
-				return;
-			}
-			console.log('Post Id: ' + res.id);
-		});
+	//Load the request module
+	var request = require('request');
+	//Lets try to make a HTTP GET request to modulus.io's website.
+	request('https://twitter.com/intent/tweet?url=http%3A%2F%2Flive.amcharts.com%2FmM1OD%2F&text=Build+your+own+free+chart+like+this+and+share+on+Twitter+or+add+as+interactive+widget+to+your+website.', function (error, response, body) {
+			console.log("blabla"+error); 
+   	 if (!error && response.statusCode == 200) {
+        	console.log("body"+body); // Show the HTML for the Modulus homepage.
+    	}
 	});
-	//
+	// var req = https.request({
+ //    host: 'graph.facebook.com',
+ //    path: '/me/feed',
+ //    method: 'POST'
+ //  }, function(res) {
+ //  	console.log('message='+encodeURIComponent(str)
+ //    +'&access_token='+encodeURIComponent('CAACESdBRI10BAN8h9ep0GoQP3hwgBdCmycXZBWXiUMikEZBeMnQ5BCtXNgg8pW4Mts1zwzEJVFF7vjO6KMr9Oh04rH7FhLfKSanRPTB6Enus3Jo0eiN8sk0ZCxwTPl4iR6oQ465JVERz4RMZAq03tLlX1S9cSeLHVaHzhVWBbbIKZBgZBlzHkWFeDqKSiFw6h6HcHZBDof4WQZDZD'));
+ //    res.setEncoding('utf8');
+ //    res.on('data', function(chunk) {
+ //      console.log('got chunk '+chunk);
+ //    });
+ //    res.on('end', function() {
+ //      console.log('response end with status '+ res.status);
+ //    });
+ //  });
+	// console.log("ACS" + req.user.accessTokens);
+	// var FB = require('fb');
+
+	// FB.api('oauth/access_token', {
+	// 	client_id: '145452562457437',
+	// 	client_secret: '5e46e22927a26aa5d529975afed0be43',
+	// 	grant_type: 'client_credentials'
+	// }, function(res) {
+	// 	if (!res || res.error) {
+	// 		console.log(!res ? 'error occurred' : res.error);
+	// 		return;
+	// 	}
+
+	// 	var accessToken = res.access_token;
+
+	// 	console.log("TESTT:"+accessToken);
+	// 	FB.setAccessToken(accessToken);
+
+	// 	var body = 'My first post using facebook-node-sdk';
+	// 	FB.api('me/feed', 'post', {
+	// 		message: body
+	// 	}, function(res) {
+	// 		console.log("TESTT:"+res.error);
+	// 		if (!res || res.error) {
+	// 			console.log(!res ? 'error occurred' : res.error);
+	// 			return;
+	// 		}
+	// 		console.log('Post Id: ' + res.id);
+	// 	});
+	// });
+	// //
 	// accessToken = req.user.accessTokens;
 	// postToFacebook('test from my personal server');
 });
